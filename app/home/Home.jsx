31a6f9525b6a7d416be35e5ac3142048a735dc40
Gabriele Callegari <g.callegari@itsrizzoli.it>
2023-06-01 16:29:40 +0200

    add: slide up

--- app/home/Home.jsx
+++ app/home/Home.jsx
@@ -1,16 +1,21 @@
-import React, { useState }  from "react";
-import { Text, View, StyleSheet, Platform, Image, Pressable } from "react-native";
+import React, { useEffect, useState }  from "react";
+import { Text, View, StyleSheet, Platform, Image, Pressable, ScrollView } from "react-native";
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import Firstpage from '../firstpage/Firstpage'
 import data from "../database/database";
+import SlidingUpPanel from 'rn-sliding-up-panel';
 
 export default function Home(){
 
     const stile = Platform.OS === 'ios' ? st.ios : st.android
 
-    const prima = data[Math.floor(Math.random() * data.length)]
+    const [prima,setPrima] = useState(data[Math.floor(Math.random() * data.length)])
 
     const [ nuovo, setNuovo] = useState(false)
+
+    const [ car , setCar ] = useState(-1)
+
+    const [altri,setAltri] = useState(data.filter(elemento => elemento.id !== prima.id))
 
     const getData = async () => {
         try {
@@ -24,7 +29,7 @@ export default function Home(){
         }
       }
 
-    useState(()=>{
+    useEffect(()=>{
         getData()
     })
 
@@ -34,8 +39,11 @@ export default function Home(){
     return(
         <View style={{flex:1, backgroundColor: 'white'}}>
             {nuovo && 
-            <View style={stile.main}>
-                <View style={stile.topCard.card} >
+            <View style={stile.main} >
+                <Pressable style={stile.topCard.card} onPress={()=>{
+                    setCar(prima)
+                    this._panel.show()
+                }} >
                     <Text style={stile.topCard.nearest}>Auto vicina</Text>
                     <Image style={stile.topCard.image}  source={prima.image}/>
                     <Text style={stile.topCard.modello}>{prima.marca} {prima.titolo}</Text> 
@@ -46,7 +54,7 @@ export default function Home(){
                         </View>
                         <Text style={{fontSize: 20, fontWeight: 'bold'}}>€{prima.prezzo}.00/h</Text>
                     </View>
-                </View>
+                </Pressable>
 
                 <View style={stile.centerCards.main}>
                     <Pressable style={stile.centerCards.card1.main}>
@@ -61,22 +69,126 @@ export default function Home(){
 
                 <View style={stile.altreAuto.main}>
                     <View style={stile.altreAuto.header}>
-                        <Text style={stile.altreAuto.headerText}>Altre auto</Text>
-                        <Text style={stile.altreAuto.headerText}>...</Text>
+                        <Text style={stile.altreAuto.headerText}>Altre auto (Scrolla)</Text>
+                        <Pressable>
+                            <Text style={stile.altreAuto.headerText}>...</Text>
+                        </Pressable>
+                        
                     </View>
+                    <ScrollView style={{width: '100%',paddingLeft: 10,paddingRight: 10, scrollbarColor: 'white', marginBottom: 10}} persistentScrollbar={true} > 
+                            {altri.map(element => {
+                                return (
+                                    <View style={stile.altreAuto.map.main} key={element.id}>
+                                        <View style={stile.altreAuto.map.top}>
+                                            <View>
+                                                <Text style={stile.altreAuto.map.title} numberOfLines={1}>{element.marca} {element.titolo}</Text>
+                                                <Text style={stile.altreAuto.map.gas} > ⛽️ {element.serbatoio}L </Text>
+                                            </View>
+                                            <Pressable onPress={()=>{
+                                                setCar(element)
+                                                this._panel.show()
+                                            }}>
+                                                <Image source={require('../../assets/arrow.png')} style={stile.altreAuto.map.image}/>
+                                            </Pressable>
+                                        </View>
+                                        <View style={stile.altreAuto.map.separator}/>
+                                    </View>
+                                )
+                            })}
+                        </ScrollView>
                 </View>
+                
             </View>
             }
 
             {!nuovo && 
                 <Firstpage callback={callback}/>
             }
+            <SlidingUpPanel ref={c => this._panel = c} showBackdrop={true} allowDragging={false} >
+                    <View style={stile.slideup1.main}>
+                        <View style={stile.slideup1.header}>
+                            <View>
+                                <Text style={stile.slideup1.text} >{car.marca} {car.titolo}</Text>
+                                <Text style={stile.slideup1.text2}>⛽️ {car.serbatoio}L</Text>
+                            </View>
+                            <Pressable onPress={() => this._panel.hide()} > 
+                                <Image source={require('../../assets/x.png')} style={stile.slideup1.image} />
+                            </Pressable>
+                        </View>
+                        <View style={stile.slideup2.main}>
+                            <Image style={stile.slideup2.image} source={car.image}/>
+                            <Text style={stile.slideup2.text}>Features</Text>
+                        </View>
+                    </View>
+            </SlidingUpPanel>
         </View>
     )
 }
 
 const st = StyleSheet.create({
     ios: {
+        slideup1:{
+            main:{
+                position: 'absolute',
+                bottom: 0,
+                left: 0,
+                right: 0,
+                height: 450,
+                backgroundColor: '#282931',
+                borderRadius: 40,
+                zIndex: 1,
+                padding: 20
+            },
+            text:{
+                color: 'white',
+                fontSize: 28,
+                fontWeight: 'bold'
+            },
+            header:{
+                width: '100%',
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                alignItems: 'center'
+            },
+            image:{
+                width: 50,
+                height: 50,
+            },
+            text2:{
+                color: 'white',
+                fontSize: 18,
+                fontWeight: 500,
+                marginTop: 5
+            },
+        },
+
+        slideup2:{
+            main: {
+                position: 'absolute',
+                bottom: 0,
+                left: 0,
+                right: 0,
+                height: 350,
+                backgroundColor: 'white',
+                borderRadius: 40,
+                zIndex: 2,
+                padding: 20
+            },
+            text: {
+                fontSize: 25,
+                fontWeight: 'bold',
+                color: 'black'
+            },
+            image:{
+                position: 'absolute',
+                top: -50,
+                width: 200,
+                height: 100,
+                right: 60,
+                resizeMode: 'contain',
+            }
+        },
+
         main:{
             width: '100%',
             height: '100%',
@@ -179,11 +291,43 @@ const st = StyleSheet.create({
                 flexDirection: 'row',
                 paddingLeft: 10,
                 paddingRight: 10,
+                paddingBottom: 10,
             },
             headerText: {
                 color: 'white',
                 fontSize: 20,
                 fontWeight: 'bold'
+            },
+            map: {
+                main: {
+                    marginTop: 10,
+                },
+                top:{
+                    justifyContent: 'space-between',
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    padding: 10
+                },
+                title: {
+                    color: 'white',
+                    fontSize: 25,
+                    fontWeight: 'bold',
+                    width: 250
+                },
+                gas:{
+                    fontSize: 20,
+                    color: 'white',
+                    marginTop: 10,
+                },
+                image: {
+                    width: 50,
+                    height: 50
+                },
+                separator: {
+                    width: '100%',
+                    height: 1,
+                    backgroundColor: 'grey'
+                }
             }
         }
 
