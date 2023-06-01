2b0aef1acd06a85645d5d40941b06bc917b0d7ea
Gabriele Callegari <g.callegari@itsrizzoli.it>
2023-06-01 12:53:22 +0200

    add :database

---
+++ app/database/database.js
@@ -0,0 +1,30 @@
+const data = [
+    {
+        titolo: '500x',
+        marca: 'Fiat',
+        serbatoio: 30,
+        kmlitro: 18,
+        prezzo: 45,
+        image: require('/Users/gabriele/Desktop/react-native/carrent/assets/500x.png')
+    },
+    {
+        titolo: 'Rav4',
+        marca: 'Toyota',
+        serbatoio: 50,
+        kmlitro: 10,
+        prezzo: 70,
+        image: require('/Users/gabriele/Desktop/react-native/carrent/assets/rav4.png')
+    },
+
+    {
+        titolo: 'Stelvio QV',
+        marca: 'Alfa Romeo',
+        serbatoio: 80,
+        kmlitro: 8,
+        prezzo: 200,
+        image: require('/Users/gabriele/Desktop/react-native/carrent/assets/stelvio.png')
+    }
+]
+
+
+export default data
