b9e8c7d167586244373f44b28189d3c614bab3e7
Gabriele Callegari <g.callegari@itsrizzoli.it>
2023-06-01 16:29:29 +0200

    add: slideup panel

--- app/database/database.js
+++ app/database/database.js
@@ -5,7 +5,9 @@ const data = [
         serbatoio: 30,
         kmlitro: 18,
         prezzo: 45,
-        image: require('/Users/gabriele/Desktop/react-native/carrent/assets/500x.png')
+        image: require('/Users/gabriele/Desktop/react-native/carrent/assets/500x.png'),
+        id: 1,
+        carburante: 'benzina'
     },
     {
         titolo: 'Rav4',
@@ -13,7 +15,9 @@ const data = [
         serbatoio: 50,
         kmlitro: 10,
         prezzo: 70,
-        image: require('/Users/gabriele/Desktop/react-native/carrent/assets/rav4.png')
+        image: require('/Users/gabriele/Desktop/react-native/carrent/assets/rav4.png'),
+        id: 2,
+        carburante: 'diesel'
     },
 
     {
@@ -22,7 +26,64 @@ const data = [
         serbatoio: 80,
         kmlitro: 8,
         prezzo: 200,
-        image: require('/Users/gabriele/Desktop/react-native/carrent/assets/stelvio.png')
+        image: require('/Users/gabriele/Desktop/react-native/carrent/assets/stelvio.png'),
+        id: 3,
+        carburante: 'benzina'
+    },
+
+    {
+        titolo: 'Giulia',
+        marca: 'Alfa Romeo',
+        serbatoio: 60,
+        kmlitro: 18,
+        prezzo: 70,
+        image: require('/Users/gabriele/Desktop/react-native/carrent/assets/giulia.png'),
+        id: 4,
+        carburante: 'diesel'
+    },
+
+    {
+        titolo: 'X3',
+        marca: 'BMW',
+        serbatoio: 60,
+        kmlitro: 15,
+        prezzo: 75,
+        image: require('/Users/gabriele/Desktop/react-native/carrent/assets/x3.png'),
+        id: 5,
+        carburante: 'benzina'
+    },
+
+    {
+        titolo: 'Q3',
+        marca: 'Audi',
+        serbatoio: 50,
+        kmlitro: 18,
+        prezzo: 55,
+        image: require('/Users/gabriele/Desktop/react-native/carrent/assets/q3.png'),
+        id: 6,
+        carburante: 'benzina'
+    },
+
+    {
+        titolo: 'GLA',
+        marca: 'Mercedes-Benz',
+        serbatoio: 40,
+        kmlitro: 19,
+        prezzo: 50,
+        image: require('/Users/gabriele/Desktop/react-native/carrent/assets/gla.png'),
+        id: 7,
+        carburante: 'diesel'
+    },
+
+    {
+        titolo: 'GLC',
+        marca: 'Mercedes-Benz',
+        serbatoio: 70,
+        kmlitro: 19,
+        prezzo: 70,
+        image: require('/Users/gabriele/Desktop/react-native/carrent/assets/glc.png'),
+        id: 8,
+        carburante: 'diesel'
     }
 ]
 
